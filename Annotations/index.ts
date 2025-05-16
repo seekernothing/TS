e9235ff06f4a03ea1c108b6ea076d9826010cd97
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-05-17 02:59:33 +0530

    initial commit

---
+++ Annotations/index.ts
@@ -0,0 +1,31 @@
+// Annotations in TS
+
+// Annotations are used to specify the data types of the variable
+
+
+// //String//
+
+
+// let myName: string = "abhishek";
+// console.log(myName)
+
+
+// let myName: string = "abhishek";
+// myName= "Virat kohli"
+// console.log(myName);
+
+
+// //Numbers//
+
+// let favNumber:number = 8
+// console.log(favNumber);
+
+
+// let favNumber: number = 8;
+// favNumber=9
+// console.log(favNumber);
+
+// //Boolean//
+
+// let isTsHard:boolean=false
+// console.log(isTsHard)
